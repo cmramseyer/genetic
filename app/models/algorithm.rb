f1a17249f5978bf7ed70d37568e259b788cbbfe0
carlos <cmramseyer@gmail.com>
2022-03-24 00:38:04 -0300

    testing logic

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -8,9 +8,19 @@ class Algorithm
   end
 
   def basic
+    population = Population.new(20)
     GENERATIONS.times do
-      
+      population.setup_selection
+      new_childs = []
+      (POPULATION/2).times do
+        chromo1 = population.select_one
+        chromo2 = population.select_one
+        child1, child2 = CrossOver.between(chromo1, chromo2)
+        new_childs << chromo1 << chromo2
+      end
+      population.chromosomes = new_childs
     end
+    population.the_best
     
   end
   
--- app/models/population.rb
+++ app/models/population.rb
@@ -3,16 +3,29 @@ class Population
   attr_accessor :chromosomes
 
   def initialize(size)
+    @size = size
     @chromosomes = size.times { Chromosome.new }
   end
 
-  def selection
-    chromosomes_evaluated = @chromosomes.map do |chromo|
+  def setup_selection
+    @chromosomes_evaluated = @chromosomes.map do |chromo|
       { chromosome: chromo, evaluation: chromo.evaluate }
     end
-    evaluation_sum = chromosomes_evaluated.sum {|h| h[:evaluation]}
-    
-    chromosome_evaluated.sort_by { |h| h[:evaluation] }.take(length/2).map { |h| h[:chromosome] }
+    @evaluation_sum = @chromosomes_evaluated.sum {|h| h[:evaluation]}
   end
+
+  def select_one
+    threshold = rand(0..@evaluation_sum)
+    acum = 0
+    @size.times do |i|
+      acum += @chromosomes_evaluated[i][:evaluation]
+      return @chromosomes_evaluated[i][:chromosome] if acum >= threshold
+    end
+  end
+
+  def the_best
+    @chromosomes_evaluated.sort_by {|h| h[:evaluation]}.last[:chromosome]
+  end
+  
   
 end
